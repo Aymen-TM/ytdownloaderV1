3428e4bf4fed4729224aea40b263d2f320e5a65d
aymenTM <t_mustapha_aymen@outlook.com>
2022-04-12 00:26:58 +0100

    fix

--- components/SearchUrl.js
+++ components/SearchUrl.js
@@ -8,9 +8,9 @@ import { useForm } from 'react-hook-form'
 function SearchUrl({show,setload,load}) {
   const {register,handleSubmit,getValues,formState: { errors }} = useForm()
 
-  const search_click = (event,url)=>{
+  const search_click = (event,url,error)=>{
     
-    if(url){
+    if(url && !error){
       show(url)
     }else{
       setload(false)
@@ -34,9 +34,9 @@ function SearchUrl({show,setload,load}) {
                               />
                           <Input type='url' {...register('url',{required:true,pattern:/https:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z]+([0-9]+[a-zA-Z]+)+)/i})}  fontWeight="bold" color='black' fontSize={["12px","16px"]} placeholder='Paste Youtube link' autoComplete="none" borderRadius="none" border="1px" borderColor='gray.400'  />
                     </InputGroup>
-                  <Button type='submit'  onClick={(e)=>{ search_click(e,getValues('url')) }}  rightIcon={<ArrowForwardIcon />} colorScheme='red' borderRadius="none" variant='solid'> Start</Button>
+                  <Button type='submit'  onClick={(e)=>{ search_click(e,getValues('url'),errors.url) }}  rightIcon={<ArrowForwardIcon />} colorScheme='red' borderRadius="none" variant='solid'> Start</Button>
           </Flex>
-          <FormErrorMessage  width="max-content" mx="auto" mt={3} >Pleas make sure your input match this pattern https://www.youtube.com/watch?v=.....</FormErrorMessage>
+          <FormErrorMessage  width="fit-content"  maxW="xl" mx="auto" mt={3} >Pleas make sure your input match this pattern https://www.youtube.com/watch?v=.....</FormErrorMessage>
       </FormControl>
     </form>
         
